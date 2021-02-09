37924034e132ee9f127776829e1d20e0d37c4fc4
Karl Heitmann <khf1988@gmail.com>
2021-02-09 18:35:07 -0300

    agregado el navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'dashboard/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'dashboard#home'
+  # root to: 'dashboard#home'
+  root to: 'rooms#index'
 end
