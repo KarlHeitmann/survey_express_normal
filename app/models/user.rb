e40ebdf269cfb6116dc1fbfbff8c5e88a31d4491
Karl Heitmann <khf1988@gmail.com>
2021-02-12 17:24:55 -0300

    ajustado gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
 
   has_many :room_messages, dependent: :destroy, inverse_of: :room
 
-
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 
 end
