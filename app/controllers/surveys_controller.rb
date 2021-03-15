49624bedfc52744db2f4deb03cf1f3eabe599b57
Karl Heitmann <khf1988@gmail.com>
2021-03-14 23:08:51 -0300

    muestra el link para ir al index de surveys

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,7 +2,8 @@ class SurveysController < ApplicationController
   before_action :set_room
   def new
     @survey = Survey.new
-
+    # @group.crews.build
+    @survey.alternatives.build
   end
 
   def show
@@ -10,9 +11,27 @@ class SurveysController < ApplicationController
 
   def index
   end
+
+  def create
+    @survey = @room.surveys.build(survey_params)
+    if @survey.save
+      puts "GUARDADO"
+      redirect_to room_surveys_path(@room)
+    else
+      puts "RENDER"
+      ap @survey
+      # ap @survey.errors
+      render :new
+    end
+  end
+
   private
 
     def set_room
       @room = Room.find(params[:room_id])
+    end
+
+    def survey_params
+      params.require(:survey).permit(:name, alternatives_attributes: [:name])
     end
 end
