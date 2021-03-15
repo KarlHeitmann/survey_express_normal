b21f33fe09002e61be1784530857095f67ce92c5
Karl Heitmann <khf1988@gmail.com>
2021-03-15 01:52:17 -0300

    creada la vista de rooms#chat

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -2,8 +2,18 @@ class RoomMessage < ApplicationRecord
   belongs_to :room
   belongs_to :user, inverse_of: :room_messages
 
+  after_create_commit :broadcast_to_room
+
   def as_json(options)
     super(options).merge(user_avatar_url: user.gravatar_url)
   end
+
+  private
+
+    def broadcast_to_room
+      room = self.room
+      # broadcast_append_to "#{room.id}"
+      broadcast_append_to "room#{room.id}"
+    end
 
 end
