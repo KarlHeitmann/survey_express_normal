c8bd26b029fd1c0ff12375d193954a709a27de9a
Karl Heitmann <khf1988@gmail.com>
2021-03-15 02:30:00 -0300

    funcionaaaaa

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -5,7 +5,6 @@ class RoomMessagesController < ApplicationController
     @room_message = RoomMessage.create user: current_user,
                                        room: @room,
                                        message: params.dig(:room_message, :message)
-    RoomChannel.broadcast_to @room, @room_message
     # begin
     #   RoomChannel.broadcast_to @room, @room_message
     # rescue Exception => e
